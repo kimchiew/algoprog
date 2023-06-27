1901aed48b26e8099588ea8034858b06df50692a
wlpasd <117998756+kimchiew@users.noreply.github.com>
2023-06-28 02:26:37 +0300

    Add files via upload

---
+++ C++/1/A1/Arithmetic operations/AfterAndBefore.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+    int N;
+    cin >> N;
+    cout << "The next number for the number " << N << " is " << N + 1 << ".\n";
+    cout << "The previous number for the number " << N << " is " << N - 1 << ".";
+    
+    return 0;
+}
